95e9c6c69ddb2adf59238fd1cadf371cd0f9c71e
rahulsogani74 <131875485+rahulsogani74@users.noreply.github.com>
2023-09-03 11:13:21 +0530

    Update FlattenBinaryTreeToLinkedList.java

--- FlattenBinaryTreeToLinkedList.java
+++ FlattenBinaryTreeToLinkedList.java
@@ -1,48 +1,45 @@
-import javax.swing.tree.TreeNode;
-import java.util.*;
-public class FlattenBinaryTreeToLinkedList {
+class FlattenBinaryTreeToLinkedList {
     static class TreeNode {
         int val;
         TreeNode left;
         TreeNode right;
         TreeNode(int val) {
             this.val = val;
+            this.left = null;
+            this.right = null;
         }
     }
-    public static void flatten(TreeNode root) {
-        if (root == null) {
-            return;
-        }
-        Stack<TreeNode> stack = new Stack<>();
-        stack.push(root);
-        TreeNode prev = null;
-        while (!stack.isEmpty()) {
-            TreeNode curr = stack.pop();
-            if (prev != null) {
-                prev.right = curr;
-                prev.left = null;
+    public void flatten(TreeNode root) {
+        if (root == null || (root.left == null && root.right == null)) return;
+
+        TreeNode current = root;
+        while (current != null) {
+            if (current.left != null) {
+                TreeNode pre = current.left;
+                while (pre.right != null) {
+                    pre = pre.right;
+                }
+                pre.right = current.right;
+                current.right = current.left;
+                current.left = null;
             }
-            if (curr.right != null) {
-                stack.push(curr.right);
-            }
-            if (curr.left != null) {
-                stack.push(curr.left);
-            }
-            prev = curr;
+            current = current.right;
         }
     }
     public static void main(String[] args) {
+        // Create a sample binary tree
         TreeNode root = new TreeNode(1);
         root.left = new TreeNode(2);
-        root.right = new TreeNode(3);
-        root.left.left = new TreeNode(4);
-        root.left.right = new TreeNode(5);
-        root.right.left = new TreeNode(6);
-        flatten(root);
-        TreeNode curr = root;
-        while (curr != null) {
-            System.out.print(curr.val + " ");
-            curr = curr.right;
+        root.right = new TreeNode(5);
+        root.left.left = new TreeNode(3);
+        root.left.right = new TreeNode(4);
+        root.right.right = new TreeNode(6);
+        FlattenBinaryTreeToLinkedList solution = new FlattenBinaryTreeToLinkedList();
+        solution.flatten(root);
+        TreeNode current = root;
+        while (current != null) {
+            System.out.print(current.val + " ");
+            current = current.right;
         }
     }
 }
